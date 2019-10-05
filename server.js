d15aebe99b88813045c73318a6b23bb400fce711
wangjingru <w15673142360@gmail.com>
2019-10-05 17:17:48 +0800

    save

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.use(session({
     secret: 'billy',
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({url:'mongodb://wangjingru:3030790wjl@cluster0-shard-00-02-rxamc.mongodb.net/my_web_session?authSource=admin'}),
     cookie: {
         maxAge: 18000000
     },
